5d4db7f4f2865f54ceddabacd70dce88e703108e
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-19 01:03:13 +0530

    commit

---
+++ src/school_management_system/School_Management_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school_management_system;
+
+/**
+ *
+ * @author Acer
+ */
+public class School_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
